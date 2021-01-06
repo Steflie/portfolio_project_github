bcdfa7d9c8ada359cc107db96cf24bf83eaecd33
Steflie <mccauchy@yahoo.com>
2021-01-06 19:50:47 +0200

    Resume models

--- resume_app/admin.py
+++ resume_app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Education, Jobs, Volunteer
+from .models import Education, Jobs, Volunteer, Publications, Languages
 
 # Register your models here.
 
 admin.site.register(Education)
 admin.site.register(Jobs)
 admin.site.register(Volunteer)
+admin.site.register(Publications)
+admin.site.register(Languages)
