5319802eac74455b53ac1a0d3cb5d71a36441087
Steflie <mccauchy@yahoo.com>
2021-01-08 19:07:46 +0200

    Finisht the html files for the resume app

--- resume_app/views.py
+++ resume_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Jobs, Education, Publications
+from .models import Jobs, Education, Publications, Languages, Volunteer
 
 # Create your views here.
 
@@ -7,8 +7,12 @@ def resume(request):
     jobs = Jobs.objects
     educations = Education.objects
     publications = Publications.objects
+    languages = Languages.objects
+    volunteers = Volunteer.objects
     return render(request, 'resume_app/resume.html', {
         'jobs':jobs, 
         'educations':educations,
         'publications':publications,
+        'languages':languages,
+        'volunteers':volunteers,
         })
